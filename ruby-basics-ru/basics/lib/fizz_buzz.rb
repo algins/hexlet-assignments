6606ae6207ad99f4d54bc5ebf069f398e7826987
@hexlet/cli <support@hexlet.io>
2022-12-12 20:32:18 +0200

    submit ruby-basics-ru/basics

--- ruby-basics-ru/basics/lib/fizz_buzz.rb
+++ ruby-basics-ru/basics/lib/fizz_buzz.rb
@@ -4,16 +4,16 @@
 def fizz_buzz(start, stop)
   result = []
   (start..stop).each do |num|
-    result << 
-    if (num % 3).zero? && (num % 5).zero?
-      'FizzBuzz'
-    elsif (num % 3).zero?
-      'Fizz'
-    elsif (num % 5).zero?
-      'Buzz'
-    else
-      num
-    end
+    result <<
+      if (num % 3).zero? && (num % 5).zero?
+        'FizzBuzz'
+      elsif (num % 3).zero?
+        'Fizz'
+      elsif (num % 5).zero?
+        'Buzz'
+      else
+        num
+      end
   end
   result.join ' ' unless result.empty?
 end
